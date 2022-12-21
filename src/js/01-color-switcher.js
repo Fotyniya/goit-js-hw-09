31de7a5955a9a3c9405835d1402ea58e4c1725ff
Fotyniya <s.kozachenko@ekwo.dp.ua>
2022-12-21 16:13:39 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
 
+const body = document.querySelector('body');
+const btnStartChangeColor = document.querySelector('[data-start]');
+const btnStopChangeColor = document.querySelector('[data-stop]');
+let timeInterval = null;
+
+btnStartChangeColor.addEventListener('click', () => {
+    timeInterval = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    btnStartChangeColor.setAttribute("disabled", "disabled");
+    btnStopChangeColor.removeAttribute("disabled", "disabled");
+  });
+  
+btnStopChangeColor.addEventListener('click', () => {
+    clearInterval(timeInterval);
+    btnStartChangeColor.removeAttribute("disabled", "disabled");
+    btnStopChangeColor.setAttribute("disabled", "disabled");
+});
